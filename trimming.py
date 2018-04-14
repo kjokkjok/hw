6427bc782b5b88722283285b3ea35f845f501f90
kjokkjok <36381685+kjokkjok@users.noreply.github.com>
2018-04-14 12:58:17 +0300

    Update trimming.py

--- trimming.py
+++ trimming.py
@@ -26,9 +26,13 @@ with open(args.output_file, 'w') as out:
       
         for index in range(len(seq.seq)-args.integer1+1):
             
-            if sum(seq.letter_annotations['phred_quality'][index:(index + args.integer1)])/len(seq.letter_annotations['phred_quality'][index:(index + args.integer1)]) < args.integer4:
-                SeqIO.write(seq[0:index], out, 'fastq')
-    		break	        
-       	    break
-	SeqIO.write(seq, out, 'fastq')
+            if (sum(seq.letter_annotations['phred_quality'][index:(index + args.integer1)])/len(seq.letter_annotations['phre$
 
+                SeqIO.write(seq[0:index], out, 'fastq')
+
+                break           
+
+            else:
+                continue
+            if index == len(seq.seq)-args.integer1+1:
+                SeqIO.write(seq, out, 'fastq')
